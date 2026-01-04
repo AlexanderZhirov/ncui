5bd192a78852f3b6afaa72e114f3c2a6eba6cd92
Alexander Zhirov <alexander@zhirov.kz>
2026-01-05 01:05:38 +0300

    Обновление example с проверкой исключений.

--- example/source/app.d
+++ example/source/app.d
@@ -4,7 +4,21 @@ import std.stdio : writeln;
 
 void main()
 {
-	auto config = SessionConfig(InputMode.raw, Cursor.normal, Echo.on);
-	auto session = new Session(config);
-	session.close();
+	auto config = SessionConfig(InputMode.raw, Cursor.normal, Echo.on, Keypad.on);
+	Session session;
+	try {
+		session = new Session(config);
+	} catch (Exception e) {
+		writeln("Не удалось инициализировать сессию:\n\t", e.msg);
+		return;
+	}
+	
+	try {
+		session.close();
+	} catch (Exception e) {
+		writeln("Не удалось закрыть сессию:\n\t", e.msg);
+		return;
+	}
+
+	writeln("OK");
 }
