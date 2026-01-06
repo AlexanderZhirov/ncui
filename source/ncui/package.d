2bab6fc3e9b2d984286c0151769c26a880fa3951
Alexander Zhirov <alexander@zhirov.kz>
2026-01-06 10:59:39 +0300

    Обновлен доступный список модулей в пакете.

--- source/ncui/package.d
+++ source/ncui/package.d
@@ -1,3 +1,15 @@
 module ncui;
 
+// Модули ядра
 public import ncui.core.session;
+public import ncui.core.event;
+public import ncui.core.ncwin;
+public import ncui.core.window;
+
+// Модули движка
+public import ncui.engine.ncui;
+public import ncui.engine.action;
+public import ncui.engine.screen;
+
+// Вспомогательная библиотека
+public import ncui.lib.logger;
