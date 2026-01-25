7c5cf54bf39e3ffcd7d9531fa9368f309370d14b
Alexander Zhirov <alexander@zhirov.kz>
2026-01-26 00:06:21 +0300

    Рефакторинг виджета TextBox, исправление проблем с ведущим пробелом в начале поля, применение темы оформления.

--- source/ncui/widgets/textbox.d
+++ source/ncui/widgets/textbox.d
@@ -9,16 +9,17 @@ import ncui.core.window;
 import ncui.core.event;
 import ncui.engine.screen;
 import ncui.engine.action;
+import ncui.engine.theme;
 import ncui.lib.checks;
 
-import std.string : toStringz;
+import std.string : toStringz, fromStringz;
 import std.utf : toUTF8, toUTF32;
 import std.algorithm : min;
 import std.range : repeat;
 import std.array : array;
 import std.regex : matchFirst, regex, Regex;
 
-final class TextBox : IWidget, IWidgetClosable
+final class TextBox : IWidget, IWidgetClosable, ICursorOwner
 {
 private:
 	// Поле по умолчанию активно.
@@ -39,12 +40,16 @@ private:
 	bool _inited;
 	// Позиция курсора.
 	size_t _cursorPosition;
+	// Количество введенных символов в поле.
+	size_t _length;
 	// Максимальный буфер.
 	int _buffer = 256;
 	// Флаг скрытых символов.
 	bool _hidden;
 	// Скрытый символ.
 	dchar _hiddenSymbol = '*';
+	// Фокус текущего поля.
+	bool _focused;
 
 	FIELD* _fieldLabel;
 	FIELD* _fieldInput;
@@ -62,19 +67,9 @@ private:
 		ncuiLibNotErr!form_driver_w(_form, OK, ch);
 	}
 
-	void setCursor(ScreenContext context, bool focused)
+	dchar modifyChar(dchar symbol)
 	{
-		if (!focused || !_enabled)
-		{
-			return;
-		}
-
-		// Принудительно сделать поле ввода активным.
-		ncuiLibNotErr!set_current_field(_form, _fieldInput);
-		// Установка высокой видимости.
-		ncuiNotErr!curs_set(Cursor.high);
-		// Держать курсор на форме при активированном виджете.
-		ncuiLibNotErr!pos_form_cursor(_form);
+		return _hidden ? _hiddenSymbol : symbol;
 	}
 
 	void moveCursor(size_t position)
@@ -84,12 +79,15 @@ private:
 			return;
 		}
 
-		driveRequest(REQ_BEG_LINE);
-		driveRequest(REQ_BEG_FIELD);
-
-		position = min(position, _text.length);
-
+		// Возврат курсора в начало строки.
+		// Используется данный метод, т.к. REQ_BEG_LINE/REQ_BEG_FIELD режут ведущие пробелы.
+		while (ncuiLibNotErrAny!form_driver_w([E_OK, E_REQUEST_DENIED], _form, KEY_CODE_YES, REQ_LEFT_CHAR) == E_OK) {}
 		_cursorPosition = 0;
+
+		if (position == 0)
+		{
+			return;
+		}
 
 		for (size_t i = 0; i < position; ++i)
 		{
@@ -102,13 +100,19 @@ private:
 
 	void modifyField()
 	{
-		dstring currentText = _hidden ? _hiddenSymbol.repeat(_text.length).array.idup : _text;
+		dstring currentText = _hidden ? _hiddenSymbol.repeat(_length).array.idup : _text;
 		ncuiLibNotErr!set_field_buffer(_fieldInput, 0, currentText.toUTF8.toStringz);
 	}
 
-	dchar modifyChar(dchar symbol)
+	bool allowedChar(dchar symbol)
 	{
-		return _hidden ? _hiddenSymbol : symbol;
+		if (!_hasMask)
+		{
+			return true;
+		}
+
+		dchar[1] line = symbol;
+		return !matchFirst(line[], _mask).empty;
 	}
 
 	void ensureCreated(Window window)
@@ -154,12 +158,12 @@ private:
 
 		_fields[2] = null;
 
-		// Установка атрибута фона для поля — подчеркивание.
-		ncuiLibNotErr!set_field_back(_fieldInput, A_UNDERLINE);
 		// Отключение опции автоперехода к следующему полю при заполнении.
 		ncuiLibNotErr!field_opts_off(_fieldInput, O_AUTOSKIP);
 		// Отключение статического режима поля — позволяет использовать горизонтальную прокрутку.
 		ncuiLibNotErr!field_opts_off(_fieldInput, O_STATIC);
+		// Выключить удаление ведущих пробелов.
+		ncuiLibNotErr!field_opts_off(_fieldInput, O_BLANK);
 		// Установка максимального размера текста в поле (ограничение на 255 символов).
 		ncuiLibNotErr!set_max_field(_fieldInput, _buffer);
 		// Создание формы.
@@ -172,23 +176,64 @@ private:
 		ncuiLibNotErrAny!post_form([E_OK, E_POSTED], _form);
 		// Перевод поля в режим вставки символа (insert mode).
 		driveRequest(REQ_INS_MODE);
-		// Установка текста в поле.
+		// Установка данных в поле формы.
 		modifyField();
-		// Позиция курсора в конце текста.
-		moveCursor(_text.length);
 
 		_inited = true;
 	}
 
-	bool allowedChar(dchar symbol)
+	void applyTheme(ScreenContext context, bool focused)
 	{
-		if (!_hasMask)
+		if (!_inited || _form is null || _fieldInput is null)
 		{
-			return true;
+			return;
 		}
 
-		dchar[1] line = symbol;
-		return !matchFirst(line[], _mask).empty;
+		if (_fieldLabel !is null)
+		{
+			StyleId lid;
+
+			if (!_enabled)
+			{
+				lid = StyleId.TextBoxLabelInactive;
+			}
+			else
+			{
+				lid = StyleId.TextBoxLabel;
+			}
+
+			const int lattr = context.theme.attr(lid);
+
+			ncuiLibNotErr!set_field_fore(_fieldLabel, lattr);
+			ncuiLibNotErr!set_field_back(_fieldLabel, lattr);
+		}
+
+		StyleId iid;
+
+		if (!_enabled)
+		{
+			iid = StyleId.TextBoxInputInactive;
+		}
+		else if (focused)
+		{
+			iid = StyleId.TextBoxInputActive;
+		}
+		else
+		{
+			iid = StyleId.TextBoxInput;
+		}
+
+		const int attr = context.theme.attr(iid);
+
+		ncuiLibNotErr!set_field_fore(_fieldInput, attr);
+		ncuiLibNotErr!set_field_back(_fieldInput, attr);
+
+		// Не перерисовывать курсор лишний раз, если поле активно.
+		if (_focused != focused)
+		{
+			moveCursor(_cursorPosition);
+			_focused = focused;
+		}
 	}
 
 public:
@@ -217,7 +262,8 @@ public:
 			_hasMask = false;
 		}
 
-		_cursorPosition = _text.length;
+		_length = _text.length;
+		_cursorPosition = _length;
 	}
 
 	string text()
@@ -245,15 +291,27 @@ public:
 		}
 
 		modifyField();
-		moveCursor(_cursorPosition);
+	}
+
+	// При фокусе виджет получает курсор после отрисовки всех виджетов.
+	override void placeCursor(ScreenContext context)
+	{
+		if (!_enabled || _form is null || _fieldInput is null)
+		{
+			return;
+		}
+
+		ncuiLibNotErrAny!set_current_field([E_OK, E_CURRENT], _form, _fieldInput);
+		ncuiNotErr!curs_set(Cursor.high);
+		ncuiLibNotErr!pos_form_cursor(_form);
 	}
 
 	override void render(Window window, ScreenContext context, bool focused)
 	{
 		// Создание формы и привязка к родителю.
 		ensureCreated(window);
-		// Настройка курсора.
-		setCursor(context, focused);
+		// Применение темы.
+		applyTheme(context, focused);
 	}
 
 	override ScreenAction handle(ScreenContext context, KeyEvent event)
@@ -263,6 +321,8 @@ public:
 			return ScreenAction.none();
 		}
 
+		bool validation = false;
+
 		if (event.isKeyCode)
 		{
 			switch (event.ch)
@@ -271,84 +331,87 @@ public:
 				moveCursor(0);
 				break;
 			case KEY_END:
-				moveCursor(_text.length);
+				moveCursor(_length);
 				break;
 			case KEY_LEFT:
-				if (_cursorPosition == 0)
+				if (_cursorPosition > 0)
 				{
-					break;
+					driveRequest(REQ_LEFT_CHAR);
+					--_cursorPosition;
 				}
-				driveRequest(REQ_LEFT_CHAR);
-				--_cursorPosition;
 				break;
 			case KEY_RIGHT:
-				if (_cursorPosition < _text.length)
+				if (_cursorPosition < _length)
 				{
 					driveRequest(REQ_RIGHT_CHAR);
 					++_cursorPosition;
 				}
 				break;
 			case KEY_DC:
-				if (_cursorPosition < _text.length)
+				if (_cursorPosition < _length)
 				{
-					driveRequest(REQ_DEL_CHAR);
 					_text = _text[0 .. _cursorPosition] ~ _text[_cursorPosition + 1 .. $];
-					// Если когда-нибудь сломается hidden-режим — раскомментировать.
-					// if (_hidden)
-					// {
-					// 	modifyField();
-					// 	moveCursor(_cursorPosition);
-					// }
+
+					driveRequest(REQ_DEL_CHAR);
+					--_length;
+
+					validation = true;
 				}
 				break;
 			case KEY_BACKSPACE:
-				if (_cursorPosition == 0)
+				if (_cursorPosition > 0)
 				{
-					break;
+					_text = _text[0 .. _cursorPosition - 1] ~ _text[_cursorPosition .. $];
+
+					driveRequest(REQ_DEL_PREV);
+					--_cursorPosition;
+					--_length;
+
+					validation = true;
 				}
-				driveRequest(REQ_DEL_PREV);
-				_text = _text[0 .. _cursorPosition - 1] ~ _text[_cursorPosition .. $];
-				--_cursorPosition;
 				break;
 			default:
 				// Остальные управлящие символы просто гасить.
 				break;
 			}
-
-			return ScreenAction.none();
 		}
-
-		if (event.isChar)
+		else if (event.isChar)
 		{
 			if (event.ch == 127 || event.ch == '\b')
 			{
-				// В случае, если курсор находится не в начале строки.
 				if (_cursorPosition > 0)
 				{
-					driveRequest(REQ_DEL_PREV);
 					_text = _text[0 .. _cursorPosition - 1] ~ _text[_cursorPosition .. $];
+
+					driveRequest(REQ_DEL_PREV);
 					--_cursorPosition;
+					--_length;
+
+					validation = true;
 				}
 			}
-			// Чтобы не поймать расхождение данных с формой.
-		else if (_text.length < _buffer && allowedChar(event.ch))
+			else if (_length < _buffer && allowedChar(event.ch))
 			{
-				if (_cursorPosition < _text.length)
+				if (_cursorPosition < _length)
 				{
 					_text = _text[0 .. _cursorPosition] ~ event.ch ~ _text[_cursorPosition .. $];
-					modifyField();
-					moveCursor(++_cursorPosition);
 				}
 				else
 				{
-					driveRequest(REQ_INS_MODE);
-					driveChar(modifyChar(event.ch));
 					_text ~= event.ch;
-					++_cursorPosition;
 				}
-			}
 
-			return ScreenAction.none();
+				driveChar(modifyChar(event.ch));
+				++_length;
+				++_cursorPosition;
+
+				validation = true;
+			}
+		}
+
+		if (validation)
+		{
+			driveRequest(REQ_VALIDATION);
 		}
 
 		return ScreenAction.none();
