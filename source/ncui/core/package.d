697622bc3b1d4b8a0e81e7745feb762d72cee481
Alexander Zhirov <alexander@zhirov.kz>
2026-02-06 00:55:56 +0300

    NCMenu добавлен в пакет.

--- source/ncui/core/package.d
+++ source/ncui/core/package.d
@@ -4,6 +4,7 @@ public import ncui.core.session;
 public import ncui.core.event;
 public import ncui.core.ncwin;
 public import ncui.core.ncform;
+public import ncui.core.ncmenu;
 public import ncui.core.ncpanel;
 public import ncui.core.window;
 public import ncui.core.panel;
